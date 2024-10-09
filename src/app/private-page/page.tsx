0c5685108ba83e473a7f17cedcd9c0a3dead0979
Mohsin Raza <mohsin.doer@gmail.com>
2024-10-10 02:22:48 +0500

    updated

--- src/app/private-page/page.tsx
+++ src/app/private-page/page.tsx
@@ -1,8 +1,15 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs'
+
 export default function PrivatePage() {
+  const { isLoaded, isSignedIn, user } = useUser()
+  
   return (
     <div className="w-max m-auto text-center">
       <h1 className="text-xl font-bold">Private Page</h1>
       <p>This is a private page. You can only see it if you are signed in.</p>
+      {isLoaded && isSignedIn && <p>Your first name is {user.firstName}</p>}
     </div>
   );
 }
